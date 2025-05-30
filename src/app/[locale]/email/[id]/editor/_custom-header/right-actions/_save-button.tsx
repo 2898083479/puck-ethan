4212ccb14925fad841bb35c515ace3ff01ce9c9b
ethan <2898083479@qq.com>
2025-05-30 17:58:17 +0800

    feat: update custom header

--- src/app/[locale]/email/[id]/editor/_custom-header/right-actions/_save-button.tsx
+++ src/app/[locale]/email/[id]/editor/_custom-header/right-actions/_save-button.tsx
@@ -1,12 +1,19 @@
 "use client"
 
 import { Button } from "@/components/ui/button";
+import { useCurrentLocale } from "@/locales";
 
 export const SaveButton = () => {
+
+    const lang = useCurrentLocale();
+
     return (
         <Button
             variant="secondary"
             className="text-tp hover:text-ts"
+            onClick={() => {
+                console.log(lang);
+            }}
         >
             Save
         </Button>
